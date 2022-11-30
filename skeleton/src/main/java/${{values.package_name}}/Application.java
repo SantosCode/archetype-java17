34e32560bb644509ee45f7657b7a7d969c921e98
Luis Santos <luis.santos@nfsconsultoria.com.br>
2022-11-30 14:28:28 -0300

    .

--- skeleton/src/main/java/${{values.package_name}}/Application.java
+++ skeleton/src/main/java/${{values.package_name}}/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArchetypeJava17Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
